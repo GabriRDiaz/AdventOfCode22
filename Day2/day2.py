4a59b0655424fb174a4b8d73053667422d1ff426
Gabriel <correoclasegabri@gmail.com>
2022-12-02 22:44:03 +0100

    Problem 2 solved

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,6 +1,7 @@
 f = open("./input.txt", "r")
 lines = f.readlines()
 
+#Yes, yes... I could have computed it dynamically so it´s scalable. But I´m kinda lazy, you know :p
 possiblePlay = {
     "AX": 4,
     "AY": 8,
@@ -13,8 +14,23 @@ possiblePlay = {
     "CZ": 6,
 }
 
+resultPerPlay = {
+    "AX": 3,
+    "AY": 4,
+    "AZ": 8,
+    "BX": 1,
+    "BY": 5,
+    "BZ": 9,
+    "CX": 2,
+    "CY": 6,
+    "CZ": 7,
+}
+
 totalScore = 0
+totalScorePerPlay = 0
 for line in lines:
     totalScore+= possiblePlay[line.replace(" ","").strip()]
-
-print(totalScore)
+    totalScorePerPlay+= resultPerPlay[line.replace(" ","").strip()]
+    
+print("Solution 1:",totalScore)
+print("Solution 2:",totalScorePerPlay)
