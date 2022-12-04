2d2817eb085a8d17dc63efe5176d6b8f898a1ba4
Gabriel <correoclasegabri@gmail.com>
2022-12-05 00:50:12 +0100

    Second version. First approach with sets

--- Day4/day4.py
+++ Day4/day4.py
@@ -5,17 +5,17 @@ fullOverlaps = 0
 partialOverlaps = 0  
 for line in lines:
     ranges = line.strip().split(",")
-    firstNumbers = ranges[0].split("-")
-    secondNumbers = ranges[1].split("-")
 
-    if((int(firstNumbers[0])<=int(secondNumbers[0]) and int(firstNumbers[1])>=int(secondNumbers[1]))
-    or (int(secondNumbers[0])<=int(firstNumbers[0]) and int(secondNumbers[1])>=int(firstNumbers[1]))):
+    #The value of Discrete Mathematics lessons
+    firstSet = set(range(int(ranges[0].split("-")[0]), int(ranges[0].split("-")[1])+1))
+    secondSet = set(range(int(ranges[1].split("-")[0]), int(ranges[1].split("-")[1])+1))
+    intersection = firstSet.intersection(secondSet)
+    
+    if(intersection==firstSet or intersection==secondSet):
         fullOverlaps+=1
         partialOverlaps+=1
     else: 
-        firstSet = set(range(int(firstNumbers[0]), int(firstNumbers[1])+1))
-        secondSet = set(range(int(secondNumbers[0]), int(secondNumbers[1])+1))
-        if(len(firstSet.intersection(secondSet))!=0): #The value of Discrete Mathematics lessons
+        if(len(intersection)!=0):
             partialOverlaps+=1
 
 print("Solution 1:",fullOverlaps)
